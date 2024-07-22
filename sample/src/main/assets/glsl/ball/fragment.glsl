5334a0f4bce4fb968a4c51a8dafc2345d1c301e0
jiangpengyong <56002982@qq.com>
2024-07-22 09:05:48 +0800

    feat 调整 glsl

--- sample/src/main/assets/glsl/ball/fragment.glsl
+++ sample/src/main/assets/glsl/ball/fragment.glsl
@@ -4,30 +4,31 @@ precision mediump float;
 in vec3 vPosition;
 out vec4 fragColor;
 
-const int verticalSegments = 18;   // 纵向切分数
-const int horizontalSegments = 36; // 横向切分数
+const float verticalSegments = 18.0;   // 纵向切分数
+const float horizontalSegments = 36.0; // 横向切分数
 
 vec2 calculateUV(vec3 normal) {
+    // 横向，即在 xz 面，
+    // atan(normal.z, normal.x) 计算出 normal 在 xz 面上与 (1, 0, 0) 的夹角，atan 结果为弧度
+    // 2 * PI（即 360 度），atan/ 2 * PI 范围落在 [0, 1] 区间
     float u = 0.5 + atan(normal.z, normal.x) / (2.0 * 3.14159);
-    float v = 0.5 - asin(normal.y) / 3.14159;
+    // 纵向
+    // asin(normal.y) 计算出 normal 与 xz 平面的夹角，asin 结果为弧度
+    // asin(normal.y) / 3.14159 范围落在 [-0.5, 0.5] 区间
+    float v = 0.5 + asin(normal.y) / 3.14159;
     return vec2(u, v);
 }
 
-/**
- * 计算两个向量间的夹角
- */
 vec4 calColor() {
-    vec3 point = normalize(vPosition.xyz);  // 归一化的顶点坐标
+    // 归一化顶点坐标
+    vec3 point = normalize(vPosition.xyz);
+    // 将给定的法向量转换为球体纹理坐标
+    vec2 uv = calculateUV(point);
 
-    // 计算纵向切分的颜色
-    float verticalStep = 1.0 / float(verticalSegments);
-    float v = calculateUV(point).y;
-    int verticalSegmentIndex = int(v / verticalStep);
+    int verticalSegmentIndex = int(uv.y * verticalSegments);
+    int horizontalSegmentIndex = int(uv.x * horizontalSegments);
 
-    // 计算横向切分的颜色
-    float horizontalStep = 1.0 / float(horizontalSegments);
-    float u = calculateUV(point).x;
-    int horizontalSegmentIndex = int(u / horizontalStep);
+    // 计算最终颜色
     if ((horizontalSegmentIndex + verticalSegmentIndex) % 2 == 0) {
         return vec4(128.0 / 255.0, 190.0 / 255.0, 245.0 / 255.0, 1.0);
     } else {
